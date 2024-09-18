0debda14753f83db2a868931f955329e3befd16f
Olivia Logan <oliviarlogan7@gmail.com>
2024-09-18 19:22:52 -0400

    Accept and validate player guesses

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ const guessMessage = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 
 const word = "magnolia";
+const guessedLetters = [];
 
 const placeholder = function (word) {
 const placeholdLetters = [];
@@ -20,7 +21,36 @@ placeholder(word);
 
 guessButton.addEventListener("click", function (e) {
 e.preventDefault();
-const letter = letterInput.value;
-console.log(letter);
+guessMessage.innerText = "";
+const guess = letterInput.value;
+const goodGuess = validateInput(guess);
+if (goodGuess) {
+    makeGuess(guess);
+}
 letterInput.value = "";
 });
+
+
+const validateInput = function(input) {
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input.length === 0) {
+        guessMessage.innerText = "Enter a letter";
+    } else if (input.length > 1) {
+        guessMessage.innerText = "Only enter one letter";
+    } else if (!input.match(acceptedLetter)) {
+        guessMessage.innerText = "Enter a letter from A to Z";
+    } else {
+        return input;
+    }
+};
+
+const makeGuess = function(guess) {
+    guess = guess.toUpperCase();
+    if (guessedLetters.includes(guess)) {
+        guessMessage.innerText = "Oops! You already guessed that letter!";
+    } else {
+        guessedLetters.push(guess);
+        console.log(guessedLetters);
+    }
+};
+
