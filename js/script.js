1b43d186554e372d687abdb1917980ccbe13cd2e
Olivia Logan <oliviarlogan7@gmail.com>
2024-09-24 17:03:06 -0400

    Displayed guessed letters, word in progress, and win message

--- js/script.js
+++ js/script.js
@@ -11,23 +11,23 @@ const word = "magnolia";
 const guessedLetters = [];
 
 const placeholder = function (word) {
-const placeholdLetters = [];
-for (const letter of word) {
-    placeholdLetters.push("●");
-}
-wordInProgress.innerText = placeholdLetters.join("");
+    const placeholdLetters = [];
+    for (const letter of word) {
+        placeholdLetters.push("●");
+    }
+    wordInProgress.innerText = placeholdLetters.join("");
 };
 placeholder(word);
 
 guessButton.addEventListener("click", function (e) {
-e.preventDefault();
-guessMessage.innerText = "";
-const guess = letterInput.value;
-const goodGuess = validateInput(guess);
-if (goodGuess) {
-    makeGuess(guess);
-}
-letterInput.value = "";
+    e.preventDefault();
+    guessMessage.innerText = "";
+    const guess = letterInput.value;
+    const goodGuess = validateInput(guess);
+    if (goodGuess) {
+        makeGuess(guess);
+    }
+    letterInput.value = "";
 });
 
 
@@ -50,7 +50,41 @@ const makeGuess = function(guess) {
         guessMessage.innerText = "Oops! You already guessed that letter!";
     } else {
         guessedLetters.push(guess);
-        console.log(guessedLetters);
+        showGuessedLetters();
+        updateWordProgress(guessedLetters);
     }
 };
+
+const showGuessedLetters = function() {
+    guessedLettersList.innerHTML = "";
+    for (const letter of guessedLetters) {
+        const li = document.createElement("li");
+        li.innerText = letter;
+        guessedLettersList.append(li);
+    }
+};
+
+const updateWordProgress = function(guessedLetters) {
+    const wordUpper = word.toUpperCase();
+    const wordArray = wordUpper.split("");
+    const updatedWord = [];
+    for (const letter of wordArray) {
+        if (guessedLetters.includes(letter)) {
+            updatedWord.push(letter.toUpperCase());
+        } else {
+            updatedWord.push("●");
+        }
+    }
+    wordInProgress.innerText = updatedWord.join("");
+    checkWin();
+};
+
+const checkWin = function () {
+    if (wordInProgress.innerText === word.toUpperCase()) {
+        guessMessage.classList.add("win");
+        guessMessage.innerHTML = `<p class="highlight">You guessed the correct word! Congrats!</p>`;
+    }
+};
+
+
 
